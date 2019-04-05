293ec7fa3ba5657fb2b2d99e1d0a4f70f29372c5
Jonathan Lui <jonathanlui@google.com>
2019-04-04 17:04:41 -0700

    Update quickstart.test.js

--- samples/system-test/quickstart.test.js
+++ samples/system-test/quickstart.test.js
@@ -21,8 +21,6 @@ const cp = require('child_process');
 
 const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
 
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
-
 const cmd = 'node quickstart.js';
 const cwd = path.join(__dirname, '..');
 
