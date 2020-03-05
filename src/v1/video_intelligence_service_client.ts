2cf909093c7760b9d1c7b8897a127285335415b6
summerji <summerji@google.com>
2020-03-05 14:29:15 -0800

    fixed upstream broken link

--- src/v1/video_intelligence_service_client.ts
+++ src/v1/video_intelligence_service_client.ts
@@ -317,7 +317,7 @@ export class VideoIntelligenceServiceClient {
    *   supported, which must be specified in the following format:
    *   `gs://bucket-id/object-id` (other URI formats return
    *   [google.rpc.Code.INVALID_ARGUMENT][google.rpc.Code.INVALID_ARGUMENT]). For more information, see
-   *   [Request URIs](/storage/docs/reference-uris).
+   *   [Request URIs](https://cloud.google.com/storage/docs/request-endpoints).
    *   A video URI may include wildcards in `object-id`, and thus identify
    *   multiple videos. Supported wildcards: '*' to match 0 or more characters;
    *   '?' to match 1 character. If unset, the input video should be embedded
@@ -336,7 +336,7 @@ export class VideoIntelligenceServiceClient {
    *   URIs are supported, which must be specified in the following format:
    *   `gs://bucket-id/object-id` (other URI formats return
    *   [google.rpc.Code.INVALID_ARGUMENT][google.rpc.Code.INVALID_ARGUMENT]). For more information, see
-   *   [Request URIs](/storage/docs/reference-uris).
+   *   [Request URIs](https://cloud.google.com/storage/docs/request-endpoints).
    * @param {string} [request.locationId]
    *   Optional. Cloud region where annotation should take place. Supported cloud
    *   regions: `us-east1`, `us-west1`, `europe-west1`, `asia-east1`. If no region
