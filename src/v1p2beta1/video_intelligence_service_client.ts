5ca3b7a345b8118441caa91475d2d5d7abd70679
yoshi-automation <yoshi-automation@google.com>
2020-03-21 04:44:51 -0700

    Change triggered by none of the following: This git repo (https://github.com/googleapis/nodejs-video-intelligence.git) Git repo https://github.com/googleapis/googleapis.git Git repo https://github.com/googleapis/synthtool.git

--- src/v1p2beta1/video_intelligence_service_client.ts
+++ src/v1p2beta1/video_intelligence_service_client.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ const version = require('../../../package.json').version;
  * @memberof v1p2beta1
  */
 export class VideoIntelligenceServiceClient {
-  private _descriptors: Descriptors = {page: {}, stream: {}, longrunning: {}};
+  private _descriptors: Descriptors = {page: {}, stream: {}, longrunning: {}, batching: {}};
   private _innerApiCalls: {[name: string]: Function};
   private _terminated = false;
   private _opts: ClientOptions;
