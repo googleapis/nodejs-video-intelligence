d13bb4268f3cc6a1c58e3d53cece92db26df4ef2
Alexander Fenster <fenster@google.com>
2020-04-10 18:57:29 -0700

    feat!: drop node8, remove eslint, update gax, fix generated protos, run the generator (#423)

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-'use strict';
-
-const cp = require('child_process');
-const {assert} = require('chai');
-const {describe, it} = require('mocha');
-
-const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
-
-const cmd = 'node analyze_face_detection.js';
-const file = 'resources/googlework_short.mp4';
-
-describe('analyzing faces in video', () => {
-  it('should identify faces in a local file', async () => {
-    const output = execSync(`${cmd} ${file}`);
-    assert.match(output, /Attribute/);
-  });
-});
+module.exports = {
+  ...require('gts/.prettierrc.json')
+}
