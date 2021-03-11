4541a7bf5ede271f149ad9724029bc9bdb4a61ce
Jeffrey Rennie <rennie@google.com>
2021-03-11 15:38:09 -0800

    chore: migrate to owl bot

--- owlbot.py
+++ owlbot.py
@@ -5,9 +5,6 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-AUTOSYNTH_MULTIPLE_COMMITS = True
-
-gapic = gcp.GAPICBazel()
 common_templates = gcp.CommonTemplates()
 
 versions = ["v1", "v1beta2", "v1p1beta1", "v1p2beta1", "v1p3beta1"]
@@ -18,14 +15,8 @@ order_versions = versions.copy()
 order_versions.append(order_versions.pop(
     order_versions.index(default_version)))
 
-for version in order_versions:
-    library = gapic.node_library("videointelligence", version)
-
-    # skip index, protos, package.json, and README.md
-    s.copy(library, excludes=["package.json", "README.md"])
-
 templates = common_templates.node_library(
     source_location="build/src", versions=versions, default_version=default_version)
 s.copy(templates)
 
-node.postprocess_gapic_library()
+node.postprocess_gapic_library_hermetic()
