b98670a6d56a47719e188e0dfe9222307084316a
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2021-05-03 15:19:24 -0400

    I hate Kokoro

--- .kokoro/release/docs-devsite.sh
+++ .kokoro/release/docs-devsite.sh
@@ -16,6 +16,14 @@
 
 set -eo pipefail
 
+if [[ -z "$CREDENTIALS" ]]; then
+  # if CREDENTIALS are explicitly set, assume we're testing locally
+  # and don't set NPM_CONFIG_PREFIX.
+  export NPM_CONFIG_PREFIX=${HOME}/.npm-global
+  export PATH="$PATH:${NPM_CONFIG_PREFIX}/bin"
+  cd $(dirname $0)/../..
+fi
+
 echo "in video-intelligence.sh"
 
 echo "$(pwd)"
