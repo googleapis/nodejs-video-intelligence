f05eb1f1377604e5594fa20bbf0edf82d76029a7
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2021-06-09 10:40:56 -0400

    Add debug info

--- .kokoro/release/docs-devsite.sh
+++ .kokoro/release/docs-devsite.sh
@@ -26,10 +26,14 @@ fi
 
 echo "In docs-deviste.sh in video-int."
 
+env
+
 if [[ -n $VERSION ]]; then
   echo "Checking out $VERSION in docs-deviste.sh in video-int."
   # git checkout $VERSION
 fi
+
+exit 0
 
 npm install
 npm install @google-cloud/cloud-rad@^0.2.0
