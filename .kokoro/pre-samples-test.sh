8f3905348faa1c99b1d5ce0eec5f5e45c96839c1
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2022-04-15 09:50:54 -0700

    reverted SA changes

--- .kokoro/pre-samples-test.sh
+++ .kokoro/pre-samples-test.sh
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/video-intelligence-key.json
